0212f507fa062f80a83c63bd4c71551fb2026a8c
SaranPanachai <you@example.com>
2024-09-24 16:01:04 +0700

    ccc

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Navbar from './Components/Navbar/Navbar'
 import Mainpage from './Components/Mainpage/mainpage'
 import Footer from './Components/Footer/Footer'
 import Ourcustomer from './Components/Ourcustomer/Ourcustomer'
-
+import Header from './assets/Picture/bangkoks.jpg'
 
 const App = () => {
   return (
 
-    <><div>
-      
+    <><div className='container'>
+       <img src={Header} alt="" />
      </div>
      <div>
      
